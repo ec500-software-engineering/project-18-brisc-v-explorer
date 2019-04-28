4db900a426f6cbdb7807ad3bfc875d7c90ecad38
Miguel Mark <mmark9@bu.edu>
2019-04-28 14:07:59 -0400

    Stop using SimpleHTTPServer
    This is ultimately meant to run as a single page web application
    with no web server. There might be a repository fetch server,
    however...

--- browserify.sh
+++ browserify.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 browserify js/main.js -o bundle.js
 if [ $? -eq 0 ]; then
-	python -c 'import BaseHTTPServer as bhs, SimpleHTTPServer as shs; bhs.HTTPServer(("0.0.0.0", '"$1"'), shs.SimpleHTTPRequestHandler).serve_forever()'
+	echo "bundle.js created!"
 else
 	echo "Failed to build bundle.js"
 fi
