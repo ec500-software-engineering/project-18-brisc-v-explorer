413c06516990e26890410e6bc8f45da40dc86467
Miguel Mark <mmark9@bu.edu>
2019-05-07 01:07:51 -0400

    Rename getHumanReadableSizeStr arg to numBytes
    Previous name implied a string type.

--- js/utils.js
+++ js/utils.js
@@ -2,18 +2,18 @@ function getStrCopy(str) {
     return (' ' + str).slice(1);
 }
 
-function getHumanReadableSizeStr(numStr) {
+function getHumanReadableSizeStr(numBytes) {
     var sizeStr = 'Invalid';
-    if (numStr < 0) {
+    if (numBytes < 0) {
         sizeStr = 'Invalid';
-    } else if (numStr < 10) {
-        sizeStr = (2 ** numStr).toString() + 'B';
-    } else if (numStr < 20) {
-        sizeStr = Math.floor((2 ** numStr) / 2 ** 10).toString() + 'kB';
-    } else if (numStr < 30) {
-        sizeStr = Math.floor((2 ** numStr) / 2 ** 20).toString() + 'MB';
-    } else if (numStr < 33) {
-        sizeStr = Math.floor((2 ** numStr) / 2 ** 30).toString() + 'GB';
+    } else if (numBytes < 10) {
+        sizeStr = (2 ** numBytes).toString() + 'B';
+    } else if (numBytes < 20) {
+        sizeStr = Math.floor((2 ** numBytes) / 2 ** 10).toString() + 'kB';
+    } else if (numBytes < 30) {
+        sizeStr = Math.floor((2 ** numBytes) / 2 ** 20).toString() + 'MB';
+    } else if (numBytes < 33) {
+        sizeStr = Math.floor((2 ** numBytes) / 2 ** 30).toString() + 'GB';
     }
     return sizeStr;
 }
